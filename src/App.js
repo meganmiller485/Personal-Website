dc02df046ac4d224496a725e368bf1c2b256b520
Megan Miller <meganmiller485@gmail.com>
2023-01-18 16:41:21 -0700

    worked on navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import "./App.css";
-import About from "./components/About";
-import Hero from "./components/Hero";
+import './App.css';
+import About from './components/About';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <Navbar />
       <Hero />
       <break></break>
       <break></break>
