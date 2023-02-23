69cf151e7e1cc19a825bbd7dcdfd1ae2a0b8a06d
Megan Miller <meganmiller485@gmail.com>
2023-02-23 15:41:39 -0700

    added womenGear

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import paint from '../assets/images/painbrush.png';
 import fitness from '../assets/images/fitness.png';
+import womenGear from '../assets/images/womanClimb.png';
 
 const Portfolio = () => {
   return (
@@ -54,17 +55,17 @@ const Portfolio = () => {
         <div className='singleProjContainer'>
           <img
             className='proj-Img'
-            src='https://images.squarespace-cdn.com/content/v1/609173bccf308b7f7e1122a0/1621006808313-Y5ZZ2NB7NAC0QS628AYI/Allotment+Ash+Tree+Ring+unframed+print.jpg?format=1000w'
+            src={womenGear}
             alt='img'
           ></img>
           <div className='proj-overlay'>
             <a
-              href='https://github.com/meganmiller485/wildTreeRings'
+              href='https://github.com/Womens-Gear-Trade/Womens-Gear-Trade'
               target='_blank'
               rel='noreferrer'
             >
               <div className='proj-desc'>
-                Wild Tree Rings <div id='techs-used'>React / JS</div>
+                Women's Gear Trade <div id='techs-used'>React / JS</div>
               </div>
             </a>
           </div>
